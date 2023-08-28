5dc114ab2c57679e37c2dca903157b53fd195325
SushkaRoot <sanya.melnichenko.2015@gmail.com>
2023-08-28 22:11:15 +0000

    delete main files

--- myscript.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo -n "Enter your username: "
-read username
-if [ $username = $USER ] ;
-then
-	 echo "Welcome to Ubuntu $username!"
-else
-	echo "Permision denied!"
-fi
--- myscript2.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for myval in /etc/r*; do
-if [ -d $myval ]
-then 
-	echo "$myval (dir)"
-else
-	echo "$myval"
-fi
-done
